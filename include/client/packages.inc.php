d489b0e146f0057463d3fbe5a78ea67486a6495b
Musadiq Dawood <musadiqdawood@gmail.com>
2024-04-05 23:56:06 +0500

    initial commit

---
+++ include/client/packages.inc.php
@@ -0,0 +1,19 @@
+<?php
+    return [
+        [
+            'id' => 1,
+            'name' => 'Bronze Package',
+            'price' => 49.99,
+            'description' => 'Basic package with limited features',
+        ], [
+            'id' => 2,
+            'name' => 'Silver Package',
+            'price' => 99.99,
+            'description' => 'Standard package with more features than Bronze',
+        ], [
+            'id' => 3,
+            'name' => 'Silver Package',
+            'price' => 149.99,
+            'description' => 'Premium package with all features included',
+        ]
+    ];
